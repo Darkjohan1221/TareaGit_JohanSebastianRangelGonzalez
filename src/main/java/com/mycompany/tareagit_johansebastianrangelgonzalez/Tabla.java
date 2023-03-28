44525789b312f3ea21fefa43b9525b2c9ba936c2
alumnociclo <alumnociclo@dptoinformatica.iesfuengirola1.net>
2023-03-28 21:38:34 +0200

    Primer commit

---
+++ src/main/java/com/mycompany/tareagit_johansebastianrangelgonzalez/Tabla.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tareagit_johansebastianrangelgonzalez;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Johan Sebastian Rangel González
+ */
+public class Tabla {
+
+    protected int num;
+
+    public Tabla(int num) {
+        this.num = num;
+    }
+
+    public String tablaDeMultiplicar() {
+        StringBuilder tabla = new StringBuilder();
+        for (int i = 0; i <= 10; i++) {
+            int resultado = this.num*i;
+            tabla.append(this.num+"*"+i+"="+resultado+"\n");
+        }
+        return tabla.toString();
+    }
+}
---
+++ src/main/java/com/mycompany/tareagit_johansebastianrangelgonzalez/TareaGit_JohanSebastianRangelGonzalez.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tareagit_johansebastianrangelgonzalez;
+
+/**
+ *
+ * @author Johan Sebastian Rangel González
+ */
+public class TareaGit_JohanSebastianRangelGonzalez {
+
+    public static void main(String[] args) {
+        Tabla t1 = new Tabla(7);
+        System.out.println(t1.tablaDeMultiplicar());
+    }
+}
